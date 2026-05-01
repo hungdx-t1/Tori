0f3c6f1d8965b1fb1289decf460b89c1dd7f0447
hungdx-t1 <nguyenlevinhhungnt153@gmail.com>
2026-05-01 16:38:16 +0700

    updated api version to 26.4.235 + re-import libraries

--- tori-api/src/main/java/com/dianxin/tori/api/base/Constants.java
+++ tori-api/src/main/java/com/dianxin/tori/api/base/Constants.java
@@ -2,7 +2,7 @@ package com.dianxin.tori.api.base;
 
 @SuppressWarnings("unused")
 public final class Constants {
-    public static final String TORI_SERVER_VERSION = "26.4.234";
+    public static final String TORI_SERVER_VERSION = "26.4.235";
 
     public static final int JAVA_REQUIRED_VERSION = 21;
     public static final String JDA_REQUIRED_VERSION = "6.4.1";
