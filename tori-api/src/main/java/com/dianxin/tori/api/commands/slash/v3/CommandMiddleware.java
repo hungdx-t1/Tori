faa2fdf2cbc5dff87da82faccd1a3872d5c9809f
hungdx-t1 <nguyenlevinhhungnt153@gmail.com>
2026-09-10 20:28:40 +0700

    release: removed old architecture

--- tori-api/src/main/java/com/dianxin/tori/api/commands/slash/v3/CommandMiddleware.java
+++
@@ -1,7 +0,0 @@
-package com.dianxin.tori.api.commands.slash.v3;
-
-@FunctionalInterface
-public interface CommandMiddleware {
-    /** Trả về true nếu cho phép request đi tiếp */
-    boolean handle(SlashContext ctx, ICommand command);
-}
