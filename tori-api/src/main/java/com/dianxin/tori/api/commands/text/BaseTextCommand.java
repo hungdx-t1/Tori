987b19a11c8421b9fe50e9ed57694a58a00fcb92
hungdx-t1 <nguyenlevinhhungnt153@gmail.com>
2026-09-17 18:34:25 +0700

    chore: deprecate text command registrars

--- tori-api/src/main/java/com/dianxin/tori/api/commands/text/BaseTextCommand.java
+++ tori-api/src/main/java/com/dianxin/tori/api/commands/text/BaseTextCommand.java
@@ -17,7 +17,12 @@ import java.util.List;
 
 /**
  * A statically configured base class for legacy text commands.
+ * @deprecated Discord is shifting toward Slash Commands to improve user privacy, security, and discoverability.
+ * So we need to deprecate any text command classes to align with Discord's requirements.
+ * It is recommended to use Slash command registrars for better experience.
  */
+@Deprecated
+@ApiStatus.ScheduledForRemoval(inVersion = "26.10.0")
 @ApiStatus.Obsolete(since = "Discord Message Content Intent restrictions")
 @SuppressWarnings("unused")
 public abstract class BaseTextCommand implements ITextCommand {
